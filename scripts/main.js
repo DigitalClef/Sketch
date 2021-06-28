8ca2f7f7c2434deca65a504e80aa7d782f7aa2db
Christian Alamillo <calam002@ucr.edu>
2021-06-27 23:58:04 -0700

    Prompt user to input dimension and added onmouseover event listener to change color

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,38 @@
 const button = document.querySelector('button');
+const sketchMain = document.querySelector('.sketch-main');
+let randColor = Math.floor(Math.random()*16777215).toString(16);
+console.log(randColor);
 
-function setGrid(size) {
-    const sketchMain = document.querySelector('.sketch-main');
-        
+//used to set up grid; dimensions will be generated based on parameter(size x size)
+function setGridandColor(size) {    
     for (let i = 0; i < (size ** 2); i++){
         const innerSketch = document.createElement('div')
         innerSketch.classList.add('inner-sketch')
         sketchMain.appendChild(innerSketch);
-        sketchMain.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    }    
+        sketchMain.style.gridTemplateColumns = `repeat(${size}, 1fr)`;  
+        
+        innerSketch.onmouseover = function() {
+            this.style.backgroundColor = `#${randColor}`;
+        }
+    }   
+    
+}
+//empty all child elements in the main div; .sketch-main
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-setGrid(8);
+//prompts user to give a number to generate grid
+button.onclick = function() {
+    removeAllChildNodes(sketchMain);
+    userValue = prompt('Enter a positive integer');
+    setGridandColor(userValue);
+}
 
 
 
 
-    
+
+
